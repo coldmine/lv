62e84506b03d781bff51adaeb8284f738dbcd70c
kim yongbin <kybinz@gmail.com>
2016-08-15 20:28:46 +0900

    atom_parse.go can read all atoms now

--- sample/atom_parse.go
+++ sample/atom_parse.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -9,31 +8,51 @@ import (
 )
 
 type Atom struct {
-	length int
-	typ    string
+	size int
+	typ  string
 	// data []byte
 }
 
 func main() {
-	f, err := os.Open("prores422hq.mov")
+	atoms := make([]Atom, 0)
+
+	pth := "prores422hq.mov"
+	f, err := os.Open(pth)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Could not open file: ", err)
+	}
+	fi, err := os.Stat(pth)
+	if err != nil {
+		log.Fatal("Could not get file info: ", err)
 	}
 
-	sizeByte := make([]byte, 4)
-	_, err = f.Read(sizeByte)
-	if err != nil {
-		log.Fatal(err)
-	}
-	n32 := binary.BigEndian.Uint32(sizeByte)
-	n := int(n32)
-	fmt.Println(n)
+	for {
+		sizeByte := make([]byte, 4)
+		_, err = f.Read(sizeByte)
+		if err != nil {
+			log.Fatal("Could not read atom size: ", err)
+		}
+		n32 := binary.BigEndian.Uint32(sizeByte)
+		n := int(n32)
 
-	typeByte := make([]byte, 4)
-	_, err = f.Read(typeByte)
-	if err != nil {
-		log.Fatal(err)
+		typeByte := make([]byte, 4)
+		_, err = f.Read(typeByte)
+		if err != nil {
+			log.Fatal("Could not read atom type: ", err)
+		}
+		t := string(typeByte)
+		atoms = append(atoms, Atom{size: n, typ: t})
+
+		// seek next atom
+		off, err := f.Seek(int64(n-8), 1)
+		if off == fi.Size() {
+			// reached at EOF
+			break
+		}
+		if err != nil {
+			log.Fatal("Could not seek to next atom: ", err)
+		}
 	}
-	t := string(typeByte)
-	fmt.Println(t)
+
+	fmt.Println(atoms)
 }
